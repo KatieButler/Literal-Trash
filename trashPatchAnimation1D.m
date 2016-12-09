56539fe4baecd30d1eeb7b9331c3936d6e6bea2d
Liv Kelley <Liv.Kelley@students.olin.edu>
2016-12-09 18:33:55 -0500

    Uploaded initial 1D code for the model!
    Currently it does not involve divgrad.

---
+++ trashPatchAnimation1D.m
@@ -0,0 +1,44 @@
+function trashPatchAnimation1D()
+%% For Liv's Sanity 
+clf 
+
+%% Variables 
+westPatchShorePollutionRate = 4064.18; %kg/year  
+
+oceanDebrisWestPollutionRate = 2177.24; %kg/year
+
+% eastPatchShorePollutionRate = 290.30; %kg/year 
+% 
+% oceanDebrisEastPollutionRate = 2177.24; %kg/year
+
+diffusion = 0.005425; %kg/year; 
+
+westPatch = 14*16329325.33; %Size in kg
+
+% eastPatch = 16329325.33; %Size in kg 
+
+currentFromWestToEast = (21444.5*5.25); % Averaged current rates in km/year
+                                      %Converted into kg/year using density
+
+currentFromEastToWest = (1576.8*5.25); % Averaged current rates in km/year
+                                     % Converted into kg/year using density
+                                     
+%time = 1; % Current time (future iterations will be in years from present.)
+
+
+%% Running code 
+
+          
+        for i=1:10; plot(westPatch(i, 1:10), 'o'); axis([0 10 0 10]); 
+        westPatch = (westPatch + westPatchShorePollutionRate + oceanDebrisWestPollutionRate)- (diffusion + currentFromWestToEast) + currentFromEastToWest; 
+        print(westPatch); 
+        end
+        hold on 
+
+%         for i=1:10; subplot(eastPatch(i, 1:10), 'r'); axis([0 10e+9 0 10]); 
+%         eastPatch = (eastPatch + eastPatchShorePollutionRate + oceanDebrisEastPollutionRate)- (diffusion + currentFromEastToWest) + currentFromWestToEast; 
+%         end
+%         hold on
+        
+
+end 
