d1ce61345d4dc68f1d23ceb0e3f33f0f0d3972bd
KatieButler <katiethetraveller@gmail.com>
2016-12-13 15:00:45 -0500

    the sink moves around and diffuses!
    particles  don't work but we're ignoring that xp

--- FinalAnimation.m
+++ FinalAnimation.m
@@ -1,4 +1,4 @@
-     
+
 function FinalAnimation () 
     %% V 
     X3 = linspace(-2,2); %Creating a space for the vector field to exist in.
@@ -8,7 +8,8 @@ function FinalAnimation ()
     v = .5*xTemp; %Setting up the j portion of the vector field (which measures density.) 
 
  %% DivGrad - Grad*V    
-
+     dx2 = 0;
+     dy2 = 0;
      X2 = linspace(-2, 2); %X boundaries
      Y2 = X2; %Setting Y boundaries using the X boundaries.
      U20 = zeros(100,100); %Creating a matrix for the output.
@@ -34,11 +35,18 @@ function FinalAnimation ()
            end 
            concentrationdT = reshape(concentrationdT,[10000, 1]); % Putting everything back into vector form for ODE45
      end 
-     [t,concentrationArray] = ode45(@Divgrad2, linspace(0,99), U20); %Taking ODE45 to solve the differential equation.
+ 
+     [t,concentrationArray] = ode45(@Divgrad2, linspace(0,100), U20); %Taking ODE45 to solve the differential equation.
+     for i = 1: length(t) 
+         axis([-1 1 -1 1 -3 3]); %axis([xMin xMax yMin yMax]); 
+         pause(.1);
+         U1 = reshape(concentrationArray(i,:), [100 100]);
+         surf(linspace(-2,2),linspace(-2,2),U1,'EdgeColor','none')
+     end
      
-      xp0 = 0; %Setting initial values 
+      xp0 = 0; %Setting initial values
       yp0 = 0;
-      
+     
       stateVectorInit = [xp0;yp0]; %The x and y of the particle being tracked.
       
      function outcome = Grady(t, stateVector) %Setting up the grad function so we can take grad of the outcome of divgrad - gradV
@@ -47,32 +55,29 @@ function FinalAnimation ()
         tIndex = int16(t + 1); %Making it so we can index through t later.
         if tIndex > 100; % Making sure things stay in bounds
             tIndex = 100;
-        end 
-        if tIndex < 1 
+        elseif tIndex < 1 
             tIndex = 1;
         end 
         U = concentrationArray(tIndex,:); %Setting up an iterating array
         U = reshape(U, 100, 100); %Converting into a matrix again.
+        
         xIndex = int16(((x/4)*100)+50); %Mapping from a (-2,2) coordinate system to a (0, 100) coordinate system.
-        yIndex = int16 ((y/4)*100)+50;
+        yIndex = int16(((y/4)*100)+50);
+        
         if xIndex < 1 %Making sure x and y are in bounds
             xIndex = 1; 
+        elseif xIndex > 100 
+            xIndex = 100;
         end
         
         if yIndex > 100 
             yIndex = 100; 
+        elseif yIndex < 1 
+            yIndex = 1;
         end 
-        
-        if xIndex > 100 
-            xIndex = 100;
-        end 
-        
-        if yIndex < 1 
-            yIndex = 1; 
-        end 
-        
-        gradU = gradient(U); %Setting up an equation that takes the gradient of U (our matrix.)
-        arbitrary = -gradU(xIndex,yIndex) + [u(xIndex, yIndex);v(xIndex,yIndex)];  %Adding velocity to -grad.
+                
+%         gradU = gradient(U); %Setting up an equation that takes the gradient of U (our matrix.)
+        arbitrary = -[dx2(xIndex);dy2(yIndex)] + [u(xIndex, yIndex);v(xIndex,yIndex)];  %Adding velocity to -grad.
         dx3 = arbitrary(1); %Formatting things so that dx3 and dy3 outcomes can be put into a vector.
         dy3 = arbitrary(2);
         outcome = [dx3; dy3]; %Making the outcome a vector so it can be processed by ODE45
@@ -82,7 +87,7 @@ function FinalAnimation ()
     % multiplication 
     %/4 * 100 + 50 
     [X, Y] = meshgrid(linspace(-2, 2), linspace(-2, 2)); %Setting up a place for the final calculations to occur. 
-    [t1,y1] = ode45(@Grady, [0 100], stateVectorInit); %Perforing ODE45 on the vector.
-    comet(y1(:,1), y1(:,2)); %Trying to animate the outcome as a vector field.
+    [t1, y1] = ode45(@Grady, [0 100], stateVectorInit); %Perforing ODE45 on the vector.
+
 end 
-    
+   
